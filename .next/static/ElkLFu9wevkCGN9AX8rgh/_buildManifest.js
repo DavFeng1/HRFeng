b6b3b3d3cad44585f3e52536b00b2469bdbefd25
David Feng <fenghourun@gmail.com>
2021-12-29 04:45:49 -0500

    Init commit

--- .next/static/ElkLFu9wevkCGN9AX8rgh/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b282b959608f58d4.css","static/chunks/pages/index-afd55e98aa19f8e1.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
