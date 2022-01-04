2aaf34b6359377e0fefad336819112d9080049e2
David Feng <fenghourun@gmail.com>
2022-01-04 13:38:17 -0500

    Fix build

--- src/assets/text/blochSphereText.ts
+++ src/assets/text/blochSphereText.ts
@@ -1,4 +1,4 @@
-export default `
+const text = `
 In quantum mechanics and computing, the Bloch sphere is a geometrical representation
 of the pure state space of a two-level quantum mechanical system (qubit), named after
 the physicist Felix Bloch.\n\n
@@ -67,3 +67,5 @@ text, and a search for 'lorem ipsum' will uncover many web sites still in their
 infancy. Various versions have evolved over the years, sometimes by accident,
 sometimes on purpose (injected humour and the like).
 `;
+
+export default text;
--- src/pages/snake.tsx
+++ src/pages/snake.tsx
@@ -1,10 +1,7 @@
-import SnakeGame from '../engines/SnakeEngine';
-
 const Snake = () => {
   return (
     <div>
       <h5> snek </h5>
-      <SnakeGame />
     </div>
   );
 };
