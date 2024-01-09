d56226016c873846c8154517fbb6208638216e14
David Feng <fenghourun@gmail.com>
2024-01-09 00:08:49 -0500

    Get latex working

--- src/pages/blog/Blog.tsx
+++ src/pages/blog/Blog.tsx
@@ -1,9 +1,17 @@
-import { useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import Markdown from 'react-markdown'
+import rehypeKatex from 'rehype-katex'
+import remarkMath from 'remark-math'
+import 'katex/dist/katex.min.css' // `rehype-katex` does not import the CSS for you
+
+import qftPath from './qft.md'
 
 import '@pages/blog/Blog.scss';
+const MathJax = require('react-mathjax');
 
 const Blog = (): JSX.Element => {
+    const [qftMd, setQftMd] = useState('');
+
     useEffect(() => {
         console.log('pages/landing/Blog.tsx ==> Component Mounted');
 
@@ -12,18 +20,19 @@ const Blog = (): JSX.Element => {
         };
     }, []);
 
+    fetch(qftPath).then((response) => response.text()).then((text) => {
+      setQftMd(text)
+    });
+
     return (
-    <div className="page-container">
-      <div id="blog-container">
-        <p>
-          These are my personal notes and problem set solutions I've accumulated as an ongoing effort to study various topics in mathematics and high energy physics
-        </p>
-          <h3> Quantum Field Theory </h3>
+      <div className="page-container">
+        <div id="blog-container">
           <p>
-            Quantum Field Theory (following the standard text 'Introduction to Quantum Field Theory' by Peskin & Schroeder)
+            These are my personal notes and problem set solutions I've accumulated as an ongoing effort to study various topics in mathematics and high energy physics
           </p>
+            <Markdown remarkPlugins={[remarkMath]} rehypePlugins={[rehypeKatex]}>{qftMd}</Markdown>
+        </div>
       </div>
-    </div>
     )
 }
 
---
+++ src/pages/blog/qft.md
@@ -0,0 +1,15 @@
+# Quantum Field Theory
+
+Following the standard text 'Introduction to Quantum Field Theory' by Peskin & Schroeder
+
+
+## Problems
+
+### 3.1 Lorentz group
+
+$$
+  [ J^{\mu \nu}] 
+$$
+
+
+
